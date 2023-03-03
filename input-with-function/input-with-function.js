e442dd097f83a1d8affad6b839081b8f2e4ede81
Karlsson0214 <karlsson0214@gmail.com>
2023-03-03 10:52:07 +0100

    comments

--- input-with-function/input-with-function.js
+++ input-with-function/input-with-function.js
@@ -1,7 +1,10 @@
 let input_element = document.getElementById("js-input");
 
-// https://developer.mozilla.org/en-US/docs/Web/API/Element/keydown_event
+// https://developer.mozilla.org/en-US/docs/Web/API/Element/keydown_event'
+// lyssnar på att knapp trycks ner
 input_element.addEventListener('keydown', function (e) {
+    // variabeln e innehåller information om keydown-händelsen
+    // körs när tangent trycks ner
     console.log("tryckt " + e.key);
     if (e.key === 'Enter') {
       let user_name = input_element.value;
