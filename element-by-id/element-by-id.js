ea6cf487ab407b32c72c0ea0822a413aa208b28b
Karlsson0214 <karlsson0214@gmail.com>
2023-03-03 09:17:01 +0100

    comments

--- element-by-id/element-by-id.js
+++ element-by-id/element-by-id.js
@@ -1,8 +1,23 @@
-// Hämta element i html-dokumentet som detta skript körs på.
-// Mer precist hämta elementet med angivet id.
+/* 
+Hämta element i html-dokumentet som detta skript körs på.
+Mer precist hämta elementet med angivet id.
+
+Mer om document.getElementById()
+https://developer.mozilla.org/en-US/docs/Web/API/Document/getElementById
+*/
+
 let entry_div = document.getElementById("js-entry");
 
-// number kan bli 1, 2, 3, 4, 5 eller 6
+/* 
+number kan bli 1, 2, 3, 4, 5 eller 6
+Math.random() ger slumptal som är 0.0 till nästan 1.0
+Mer om Math.radom()
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+
+Math.floor(3.14) avrundar alltid ner till närmast mindre heltal.
+Mer om Math.floor()
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/floor
+*/ 
 let number = Math.floor(Math.random() * 6 + 1);
 entry_div.innerHTML += "<p>Tärningen visar:</p>";
 entry_div.innerHTML += "<p>" + number + "</p>";
