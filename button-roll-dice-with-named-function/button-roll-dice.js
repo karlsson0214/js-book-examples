93329378338b86cccbf866b19fcf3078be69d48d
Karlsson0214 <karlsson0214@gmail.com>
2023-03-07 11:31:56 +0100

    roll dice

--- button-roll-dice-with-named-function/button-roll-dice.js
+++ button-roll-dice-with-named-function/button-roll-dice.js
@@ -1,8 +1,11 @@
 let button = document.getElementById("js-button");
 let output = document.getElementById("js-output");
 
-button.addEventListener('click', function (e) {
+button.addEventListener('click', rollDice(e));
+
+function rollDice(e)
+{
     console.log("tryckt " + e.key);
     let number = Math.floor(Math.random() * 6 + 1);
     output.innerHTML = number;
-});
+}
